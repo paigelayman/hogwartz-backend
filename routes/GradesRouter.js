1923df0eb6fb21ede0187b3a5cf28a4f8e799450
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-11-21 12:08:17 -0700

    Routes fixed & ReadMe updated

--- routes/GradesRouter.js
+++ routes/GradesRouter.js
@@ -3,8 +3,8 @@ const controller = require('../controllers/GradesController')
 
 router.get('/', controller.getGrades)
 
-router.get('/grades/:grade_id', controller.getOneGrade)
+router.get('/:grade_id', controller.getOneGrade)
 
-router.post('/grades/new', controller.createGrade)
+router.post('/new', controller.createGrade)
 
 module.exports = router
