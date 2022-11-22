81c10a972597143e022d20b2d13da8c4d4cbcc76
Paige Layman <paigelayman96@gmail.com>
2022-11-22 09:53:57 -0500

    added score and letter grade to student courses

--- migrations/20221122025117-add-grade-id-to-studentcourses.js
+++ migrations/20221122025117-add-grade-id-to-studentcourses.js
@@ -5,12 +5,12 @@ const { query } = require('express')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return queryInterface.addColumn('student_courses', 'gradeId', {
+    return queryInterface.addColumn('student_courses', 'score', {
       type: Sequelize.INTEGER
     })
   },
 
   async down(queryInterface, Sequelize) {
-    return queryInterface.removeColumn('student_courses', 'gradeId')
+    return queryInterface.removeColumn('student_courses', 'score')
   }
 }
