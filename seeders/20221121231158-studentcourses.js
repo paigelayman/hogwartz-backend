85752435e74b0a3c6ca7246f3a1548826aa343f6
Paige Layman <paigelayman96@gmail.com>
2022-11-22 11:01:12 -0500

    added update function for studentcourses and added to seed files

--- seeders/20221121231158-studentcourses.js
+++ seeders/20221121231158-studentcourses.js
@@ -7,12 +7,104 @@ module.exports = {
       {
         studentId: 1,
         courseId: 1,
+        letter: 'A',
+        score: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         studentId: 2,
-        courseId: 1,
+        courseId: 5,
+        letter: 'A',
+        score: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 3,
+        courseId: 3,
+        letter: 'B',
+        score: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 4,
+        courseId: 2,
+        letter: 'A',
+        score: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 5,
+        courseId: 4,
+        letter: 'C',
+        score: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 6,
+        courseId: 2,
+        letter: 'B',
+        score: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 7,
+        courseId: 5,
+        letter: 'A',
+        score: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 8,
+        courseId: 3,
+        letter: 'C',
+        score: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 9,
+        courseId: 7,
+        letter: 'B',
+        score: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 10,
+        courseId: 2,
+        letter: 'B',
+        score: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 11,
+        courseId: 5,
+        letter: 'F',
+        score: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 12,
+        courseId: 3,
+        letter: 'D',
+        score: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        studentId: 13,
+        courseId: 6,
+        letter: 'B',
+        score: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       }
